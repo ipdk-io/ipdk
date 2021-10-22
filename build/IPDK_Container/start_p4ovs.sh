415094e90dc59bbcb67708cae09051fb105d3796
Namrata Limaye <namrata.limaye@intel.com>
2021-10-22 00:28:32 -0700

    IPDK Container Version 0.1.0
    IPDK Container is a Virtual Networking Infrastructure Container. IPDK Container
     is built with the following components –
    1. OvS with P4 Support (https://github.com/ipdk-io/ovs/tree/ovs-with-p4)
    2. P4_DPDK_target library (https://github.com/p4lang/p4-dpdk-target)
    3. P4-DPDK (https://github.com/DPDK/dpdk)
    
    The container builds all the three components and their dependencies and
    integrates them within OvS providing a P4 based virtual networking switch
    within the container.
    
    This commit contains all the files that are needed to build the IPDK Container
    
    Signed-off-by: Namrata Limaye <namrata.limaye@intel.com>
    Title: IPDK Container

--- build/IPDK_Container/start_p4ovs.sh
+++ build/IPDK_Container/start_p4ovs.sh
@@ -1,16 +1,5 @@
-# Copyright (c) 2021 Intel Corporation.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at:
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#Copyright (C) 2021 Intel Corporation
+#SPDX-License-Identifier: Apache-2.0
 
 #!/bin/bash
 
@@ -21,9 +10,8 @@ then
     return 1
 fi
 
-#TODO: Temporary add
-export http_proxy=child-fm.intel.com:911
-export https_proxy=child-fm.intel.com:911
+#export http_proxy=<your proxy>
+#export https_proxy=<your proxy>
 
 WORKDIR=$1
 SCRIPTS_DIR=/root/scripts
@@ -57,5 +45,3 @@ build_p4sde
 install_dependencies
 build_p4c
 build_p4ovs
-
-echo "***** Next steps are in the run_ovs_cmds *****"
