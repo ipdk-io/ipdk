2000fb35ba0208762afe0f9f4fd5f284b3fecef7
Kyle Mestery <mestery@mestery.com>
2021-12-10 11:06:38 -0600

    set_hugepages.sh: Fix this to work on WSL2
    The script was failing to run because some /sys entry does not exist
    by default on WSL2. Modify this to work with what WSL2 has.
    
    Closes #18
    
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- build/IPDK_Container/scripts/set_hugepages.sh
+++ build/IPDK_Container/scripts/set_hugepages.sh
@@ -3,13 +3,17 @@
 #SPDX-License-Identifier: Apache-2.0
 
 #...Setting Hugepages...#
-mkdir /mnt/huge
+mkdir -p /mnt/huge
 mount -t hugetlbfs nodev /mnt/huge
 echo -e "nodev /mnt/huge hugetlbfs\n" >> /etc/fstab
 
 echo "vm.nr_hugepages = 4096" >> /etc/sysctl.conf
 #sysctl -p /etc/sysctl.conf
 
-echo 1024 | sudo tee /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
-echo 1024 | sudo tee /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
+if [ ! -d /sys/devices/system/node/node0/hugepages-2048kB ] ; then
+	echo 1024 | tee /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+else
+	echo 1024 | sudo tee /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
+	echo 1024 | sudo tee /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
+fi
 
