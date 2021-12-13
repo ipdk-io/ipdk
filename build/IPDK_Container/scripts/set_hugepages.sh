4afe5c9757fef93294170015fa5d95e6defc5277
Kyle Mestery <mestery@mestery.com>
2021-12-13 10:54:38 -0600

    Make directory checks more consistent
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- build/IPDK_Container/scripts/set_hugepages.sh
+++ build/IPDK_Container/scripts/set_hugepages.sh
@@ -10,10 +10,18 @@ echo -e "nodev /mnt/huge hugetlbfs\n" >> /etc/fstab
 echo "vm.nr_hugepages = 4096" >> /etc/sysctl.conf
 #sysctl -p /etc/sysctl.conf
 
-if [ ! -d /sys/devices/system/node/node0/hugepages-2048kB ] ; then
-	echo 1024 | tee /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
-else
+#
+# Check if the node version of hugepages exists, and set hugepages if so.
+#
+if [ -d /sys/devices/system/node/node0/hugepages-2048kB ] ; then
 	echo 1024 | sudo tee /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
 	echo 1024 | sudo tee /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
+fi
+
+#
+# Check if the kernel/mm version of hugepages exists, and set hugepages if so.
+#
+if [ -d /sys/kernel/mm/hugepages/hugepages-2048kB ] ; then
+	echo 1024 | tee /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
 fi
 
