ipdk-io/ipdk

commit 119c5a6cff03d677648a1d4c883ab96264d17d64

IPDK: Reduce docker memory spike
As part of previous commit, user has been given an option to
retain source code or not by tweaking KEEP_SOURCE_CODE variable.

But this cleanup does at the end after building all repositories,
With this method there will be an intermediate spike in memory
consumption up to ~13GB and then reduces to ~6.5GB or ~3.5GB based
on KEEP_SOURCE_CODE value.
This intermediate spike causes an issue on systems with lower memory.
To solve this problem, we are cleaning temporary build files generated by
P4C & p4-driver after the build and removing dependent packages source
code immediately after installation.

Change-type: DefectResolution
Title: IPDK: Reduce docker memory
Signed-off-by: n-sandeep <sandeep.nagapattinam@intel.com>

build/IPDK_Container/start_p4ovs.sh
@@ -50,6 +50,8 @@ install_dependencies() {
         cd "$WORKDIR"/P4-OVS && sed -i 's/sudo //g' install_dep_packages.sh && \
             ${SHELL_STRING} ./install_dep_packages.sh "$WORKDIR"
     fi
+    #...Removing Dependencies Source Code After Successful Installation...#
+    rm -rf ${WORKDIR}/P4OVS_DEPS_SRC_CODE || exit 1
 }
 
 build_p4c () {

build/IPDK_Container/Dockerfile
@@ -73,6 +73,9 @@ COPY ./examples /root/examples
 COPY ./start_p4ovs.sh /root/start_p4ovs.sh 
 COPY ./run_ovs_cmds /root/run_ovs_cmds
 RUN  /root/start_p4ovs.sh /root && \
-     /root/scripts/run_cleanup.sh /root ${KEEP_SOURCE_CODE}
+     /root/scripts/run_cleanup.sh /root ${KEEP_SOURCE_CODE} && \
+     dnf -y clean all && \
+     rm -rf /var/cache/yum
+
 #ADD ./start_p4ovs.sh /root/start_p4ovs
 #ENTRYPOINT exec /root/start_p4ovs.sh /root

build/IPDK_Container/scripts/build_p4c.sh
@@ -37,7 +37,7 @@ cmake -DENABLE_BMV2=OFF -DENABLE_EBPF=OFF -DENABLE_UBPF=OFF \
       -DENABLE_P4C_GRAPHS=OFF -DENABLE_P4TEST=OFF -DENABLE_GTESTS=OFF ..
 make $NUM_THREADS
 make $NUM_THREADS install
-make clean
+make $NUM_THREADS clean
 ldconfig
 
 set +e

build/IPDK_Container/scripts/build_p4sde.sh
@@ -73,6 +73,7 @@ cd "$SDE/p4-driver" || exit
 make clean
 make $NUM_THREADS
 make $NUM_THREADS install
+make $NUM_THREADS clean
 ldconfig
 
 set +e

build/IPDK_Container/scripts/run_cleanup.sh
@@ -8,9 +8,6 @@ KEEP_SOURCE_CODE=$2
 FLAG_YES="YES"
 FLAG_NO="NO"
 
-echo "Removing P4OVS dependent modules source code"
-cd "${ROOT_DIR}" &&  rm -rf P4OVS_DEPS_SRC_CODE
-
 echo "Copy p4ovs_env_setup.sh script from P4-OVS"
 cp "${ROOT_DIR}/P4-OVS/p4ovs_env_setup.sh" "${ROOT_DIR}/scripts/"
 
@@ -30,16 +27,9 @@ if [ "${KEEP_SOURCE_CODE,,}" = "${FLAG_NO,,}" ] ; then
     echo "Removing P4C source code"
     cd "${ROOT_DIR}" &&  rm -rf P4C
 
-
 elif [ "${KEEP_SOURCE_CODE,,}" = "${FLAG_YES,,}" ]; then
     echo "Make clean P4-OVS"
     cd "${ROOT_DIR}/P4-OVS" && make clean
-
-    echo "Make clean p4-driver"
-    cd "${ROOT_DIR}/p4-sde/p4-driver" && make clean
-
-    echo "Make clean P4C"
-    cd "${ROOT_DIR}/P4C/build" && make clean
 
 else
     echo "Unrecognized option for source code retention/removal: " \

commit b92f53dc9d87916d84447bcbee0d5527f776b749

shellcheck: Fix shellcheck error
119c5a6c introduced a shellcheck regression.

Signed-off-by: Kyle Mestery <mestery@mestery.com>

build/IPDK_Container/start_p4ovs.sh
@@ -51,7 +51,7 @@ install_dependencies() {
             ${SHELL_STRING} ./install_dep_packages.sh "$WORKDIR"
     fi
     #...Removing Dependencies Source Code After Successful Installation...#
-    rm -rf ${WORKDIR}/P4OVS_DEPS_SRC_CODE || exit 1
+    rm -rf "${WORKDIR}/P4OVS_DEPS_SRC_CODE" || exit 1
 }
 
 build_p4c () {