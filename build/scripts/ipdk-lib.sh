26db22d19ad36b4e6eea57d49f75deacd8fc99d5
stolsma <github@tolsma.net>
2022-02-09 16:00:39 +0100

    Improve IPDK cli and ci build/run with buildx, multiarch, artefacts and push to registries
    Implements large parts of #101
    
    Expanded IPDK CLI commands and options with:
    - Support `ipdk config [key]=[value]` for setting key=value in ~/.ipdk/ipdk.env
    - Support `ipdk build --platform [docker buildx supported platforms]` for local and multiarch builds
    - Support `ipdk build --tags [taglist] --labels [labellist] --push --export [filename]`
    - Support `ipdk start --platform [docker buildx supported platform]`
    - Support `ipdk tag [tag]` for local image builds
    - Support `ipdk export [filename]` for local image builds
    - Support `ipdk push [tag]` for local image builds
    - check for support of `docker buildx` on host system
    
    Example:
    `ipdk build --no-cache --platform linux/arm64` builds a ARM64 based ipdk image if run on a linux/amd-64 based host system
    `ipdk start --platform linux/arm64` starts a ARM64 based image on a linux/amd-64 based host system by using qemu emulation
    
    Expanded the Github `makefile.yml` action file with:
    - concurrency option to prevent running multiple build actions for the same PR or push.
    - install of qemu runtimes
    - install of buildx
    - (for now disabled) login to dockerhub
    - login to Github Container Registry (ghcr)
    - create build metadata (tags and labels)
    - use ipdk cli build command
    - push the created multiarch container images to the registries when run from push
    - upload created multiarch container images as artefacts when the action is run from PR
    - Currently only building containers for one architecture platform (linux/amd-64)
    
    Signed-off-by: stolsma <github@tolsma.net>

--- build/scripts/ipdk-lib.sh
+++ build/scripts/ipdk-lib.sh
@@ -13,6 +13,73 @@ print_banner() {
 	echo ""
 }
 
+
+function version() {
+	printf '%02d' "$(echo "$1" | tr . ' ' | sed -e 's/ 0*/ /g')" 2>/dev/null
+}
+
+#
+# Check if docker buildx is usable
+#
+function check_buildx() {
+	if ! command -v docker >/dev/null 2>&1; then
+		echo "Can't find docker. Install first!!" >&2
+		return 1
+	fi
+
+	docker_version="$(docker --version | cut -d' ' -f3 | tr -cd '0-9.')"
+	if [[ "$(version "$docker_version")" < "$(version '19.03')" ]]; then
+		error "docker $docker_version too old. Need >= 19.03"
+		return 1
+	fi
+
+	docker_experimental="$(docker version | awk '/^ *Experimental:/ {print $2 ; exit}')"
+	if [[ "$docker_experimental" != 'true' ]]; then
+		echo "docker experimental flag not enabled: Set with 'export DOCKER_CLI_EXPERIMENTAL=enabled'" >&2
+		return 1
+	fi
+
+	kernel_version="$(uname -r)"
+	if [[ "$(version "$kernel_version")" < "$(version '4.8')" ]]; then
+		echo "Kernel $kernel_version too old, need >= 4.8 to build with --platform." >&2
+		return 1
+	fi
+
+	if [[ "$(mount | grep -c '/proc/sys/fs/binfmt_misc')" == '0' ]]; then
+		echo "/proc/sys/fs/binfmt_misc not mounted." >&2
+		return 1
+	fi
+
+	if ! command -v update-binfmts >/dev/null 2>&1; then
+		echo "Can't find update-binfmts." >&2
+		return 1
+	fi
+
+	binfmt_version="$(update-binfmts --version | awk '{print $NF}')"
+	if [[ "$(version "$binfmt_version")" < "$(version '2.1.7')" ]]; then
+		echo "update-binfmts $binfmt_version too old. Need >= 2.1.7" >&2
+		return 1
+	fi
+
+	if [[ ! -e '/proc/sys/fs/binfmt_misc/qemu-aarch64' ]]; then
+		# Skip this test if QEMU isn't registered with binfmt_misc. It might
+		# come from a docker image rather than the host file system.
+		if [[ ! -e '/usr/bin/qemu-aarch64-static' ]]; then
+			echo "Missing QEMU."  >&2
+			return 1
+		fi
+	fi
+	if [[ ! -e '/proc/sys/fs/binfmt_misc/qemu-aarch64' ]]; then
+		echo "QEMU not registered in binfmt_misc." >&2
+		return 1
+	fi
+	flags="$(grep 'flags:' /proc/sys/fs/binfmt_misc/qemu-aarch64 | cut -d' ' -f2)"
+	if [[ "$(echo "$flags" | grep -c F)" == '0' ]]; then
+		echo "QEMU not registered in binfmt_misc with fix-binary (F) flag." >&2
+		return 1
+	fi
+}
+
 #
 # Replace configuration line with new line and if not exist add
 # $1 = SEARCH_FOR
