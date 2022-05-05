d99a5a65e956196bb04369b70a0e6aa95f91e1d4
Artsiom Koltun <artsiom.koltun@intel.com>
2022-05-05 10:45:51 +0200

    Remove custom hot-plug service from storage proxy.
    SMA is integrated into storage solution and used to hot-plug/unplug
    virtio-blk disks. Based on that, custom hot-plug/unplug service can
    be removed from proxy since it is not used anymore.
    
    Signed-off-by: Artsiom Koltun <artsiom.koltun@intel.com>

--- build/storage/tests/ut/run_all_unit_tests.sh
+++ build/storage/tests/ut/run_all_unit_tests.sh
@@ -13,8 +13,6 @@ fi
 ls -l /host-target/tests
 
 result=0
-python -m unittest discover -v -s /proxy-container/tests
-result=$(("$?" | "${result}"))
 python -m unittest discover -v -s /host-target/tests
 result=$(("$?" | "${result}"))
 
