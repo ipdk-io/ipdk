7cbeb458642c03c0445e9063785ca9178727dc87
nupuruttarwar <nupur.uttarwar@intel.com>
2022-07-19 14:47:38 -0700

    Checkout specific SHA for OVS (#213)
    This commit checks out specific SHA for OVS. Once TDI support
    is available in ipdk.ovs repo, this will be updated.
    
    Title: Checkout specific SHA for OVS
    Signed-off-by: Nupur Uttarwar <nupur.uttarwar@intel.com>

--- build/networking/scripts/get_p4ovs_repo.sh
+++ build/networking/scripts/get_p4ovs_repo.sh
@@ -16,4 +16,8 @@ echo "Removing P4-OVS directory if it already exits"
 if [ -d "P4-OVS" ]; then rm -Rf P4-OVS; fi
 echo "Cloning P4-OVS repo"
 cd "$WORKDIR" || exit
-git clone https://github.com/ipdk-io/ovs.git -b ovs-with-p4 --recursive P4-OVS
+git clone https://github.com/ipdk-io/ovs.git -b ovs-with-p4 P4-OVS
+pushd "$WORKDIR/P4-OVS" || exit
+git checkout abfbcb94bd899580fdf96950939c0910d489894d
+git submodule update --init --recursive
+popd || exit
