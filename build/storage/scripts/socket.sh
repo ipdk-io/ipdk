d3c81861196c07c8608cecfdc933bbc72de3d017
rilnickx <rafalx.ilnicki@intel.com>
2022-08-09 17:27:57 +0200

    Get rid of send_command_over_unix_socket_and_no_word_found
    Signed-off-by: rilnickx <rafalx.ilnicki@intel.com>

--- build/storage/scripts/socket.sh
+++ build/storage/scripts/socket.sh
@@ -18,22 +18,6 @@ print(
 EOF
 }
 
-function send_command_over_unix_socket_and_no_word_found() {
-	return $(python <<- EOF
-from scripts import socket_functions
-
-print(
-    socket_functions.send_command_over_unix_socket_and_no_word_found(
-        sock: "${1}",
-        cmd: "${2}",
-        wait_for_secs: float("${3}"),
-        word: "${4}"
-    )
-)
-EOF
-)
-}
-
 function get_output_from_unix_socket() {
 	python <<- EOF
 from scripts import socket_functions
--- build/storage/scripts/socket_functions.py
+++ build/storage/scripts/socket_functions.py
@@ -1,5 +1,4 @@
 import logging
-import re
 import socket
 
 logging.root.setLevel(logging.CRITICAL)
@@ -18,13 +17,6 @@ def send_command_over_unix_socket(sock: str, cmd: str, wait_for_secs: float) -> 
         except socket.timeout:
             logging.error("Timeout exceeded")
         return b"".join(out).decode()
-
-
-def send_command_over_unix_socket_and_no_word_found(
-    sock: str, cmd: str, wait_for_secs: float, word: str
-) -> int:
-    out = send_command_over_unix_socket(sock=sock, cmd=cmd, wait_for_secs=wait_for_secs)
-    return 1 if re.search(word, out) else 0
 
 
 def get_output_from_unix_socket(sock: str, wait_for_secs: float) -> str:
