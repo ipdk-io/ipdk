b86560d08fac58004d1e6922683ce75113cbb850
Artsiom Koltun <artsiom.koltun@intel.com>
2022-08-30 10:52:38 +0200

    Run storage cmd-sender as separate container.
    Sending cmds from test-driver confuses users.
    To avoid this commands to storage containers will be sent from
    dedicated container.
    
    Signed-off-by: Artsiom Koltun <artsiom.koltun@intel.com>
    Signed-off-by: rilnickx <rafalx.ilnicki@intel.com>

---
+++ build/storage/scripts/run_cmd_sender.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+#
+
+[ "$DEBUG" == 'true' ] && set -x
+
+export IMAGE_NAME="cmd-sender"
+export BUILD_IMAGE="true"
+scripts_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+# shellcheck source=./scripts/run_container.sh
+# shellcheck disable=SC1091,SC1090
+source "${scripts_dir}/run_container.sh"
