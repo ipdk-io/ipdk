04cc7cd3d4e3a92e0a45c461e5566686a7a1e1be
Kula <przemyslawx.kula@intel.com>
2022-09-26 19:12:02 +0200

    Test terminal connection
    Signed-off-by: Kula <przemyslawx.kula@intel.com>

--- build/storage/tests/it/ptf_tests/python_system_tools/ssh_terminal.py
+++ build/storage/tests/it/ptf_tests/python_system_tools/ssh_terminal.py
@@ -2,47 +2,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-import logging
-
 from paramiko.client import SSHClient, AutoAddPolicy
 from tenacity import retry, stop_after_attempt, stop_after_delay
 
 
 class CommandError(Exception):
     """A custom Exception of a Terminal class raised when an error during command execution occurs"""
-
-
-class BaseSSHTerminal(SSHClient):
-    def __init__(self, ip_address: str, username: str, password: str, port: int = 22):
-        super().__init__()
-        self.load_system_host_keys()
-        self.set_missing_host_key_policy(AutoAddPolicy)
-        self._ip_address = ip_address
-        self._username = username
-        self._password = password
-        self._port = port
-
-    @retry(stop=(stop_after_attempt(3) | stop_after_delay(5)), reraise=True)
-    def connect(self, *args, **kwargs):
-        """
-        Initializes an SSH connection to an SSH server
-        """
-
-        super().connect(
-            self._ip_address,
-            self._port,
-            self._username,
-            self._password,
-            *args,
-            **kwargs
-        )
-
-    def __enter__(self):
-        self.connect()
-        return self
-
-    def __exit__(self, type, value, traceback):
-        self.close()
 
 
 class SSHTerminal:
@@ -64,7 +29,7 @@ class SSHTerminal:
         raise_on_error is equal to True (default True)
     """
 
-    def __init__(self, terminal: BaseSSHTerminal):
+    def __init__(self, config, *args, **kwargs):
         """
         Parameters
         ----------
@@ -72,60 +37,36 @@ class SSHTerminal:
         A high-level representation of a session with an SSH server
         """
 
-        self._terminal = terminal
+        self.config = config
+        self.terminal = SSHClient()
+
+        self.terminal.load_system_host_keys()
+        self.terminal.set_missing_host_key_policy(AutoAddPolicy)
+        self.terminal.connect(config.ip_address, config.port, config.username, config.password, *args, **kwargs)
 
     def execute(
         self,
         cmd: str,
-        cwd: str | None = None,
-        timeout: float | None = None,
-        raise_on_error: bool = True,
+        timeout: float = None,
     ) -> tuple[str, int]:
         """
-        Executes a command on the SSH server
+        Simple function executes a command on the SSH server
 
         Parameters
         ----------
         cmd: str
             The command to execute
-        cwd: str | None
-            The directory path where command should be executed
         timeout: float | None
             Set command's channel timeout (default None)
-        raise_on_error: bool
-            Specify if error is raised when the command execution fails (default True)
 
         Returns
         -------
-        tuple(str, int)
-            The tuple of the output and return code of the executed command
-
-        Raises
-        ------
-        TerminalException
-            When the execution of the command fails and raise_on_error parameter is equal to True
+        list(str)
+            The list of the lines output
         """
 
-        if cwd is not None:
-            cmd = f"cd {cwd} && {cmd}"
-
-        logging.info(f'Execute command: "{cmd}" with timeout={timeout}')
-        with self._terminal:
-            _, stdout, stderr = self._terminal.exec_command(cmd, timeout=timeout)
-            rc = stdout.channel.recv_exit_status()
-
+        _, stdout, stderr = self.terminal.exec_command(cmd, timeout=timeout)
         #  if command is executed in the background don't wait for the output
-        out = (
-            ""
-            if cmd.rstrip().endswith("&")
-            else (stdout.read() or stderr.read()).decode()
-        )
-
-        if raise_on_error and rc:
-            raise CommandError(f"Command execution failed\nrc={rc}\nout={out}")
-        elif not raise_on_error and rc:
-            logging.error(f"Command failed with\nrc={rc}\nout={out}")
-        else:
-            logging.info(f"Command executed with\nrc={rc}\nout={out}")
-
-        return out, rc
+        out = [] if cmd.rstrip().endswith("&") else stdout.readlines() or stderr.readlines()
+        result = map(lambda x: x[:-1], out)
+        return list(result)
--- build/storage/tests/it/ptf_tests/tests/test_connection.py
+++ build/storage/tests/it/ptf_tests/tests/test_connection.py
@@ -7,15 +7,54 @@ from ptf.base_tests import BaseTest
 from python_system_tools.config import (
     StorageTargetConfig, IPUStorageConfig, HostTargetConfig,
 )
+from python_system_tools.ssh_terminal import SSHTerminal
 
 
-class TestFirstMockTest(BaseTest):
+class TestTerminalConnect(BaseTest):
 
     def setUp(self):
-        pass
+        self.storage_target_terminal = SSHTerminal(StorageTargetConfig())
+        self.ipu_storage_terminal = SSHTerminal(IPUStorageConfig())
+        self.host_target_terminal = SSHTerminal(HostTargetConfig())
 
     def runTest(self):
+        self.assertEqual(
+            self.storage_target_terminal.execute("whoami")[0],
+            self.storage_target_terminal.config.username,
+        )
+        self.assertEqual(
+            self.ipu_storage_terminal.execute("whoami")[0],
+            self.ipu_storage255.config.username,
+        )
+        self.assertEqual(
+            self.host_target_terminal.execute("whoami")[0],
+            self.host_target_terminal.config.username,
+        )
+
+    def tearDown(self):
         pass
+
+
+class TestTerminalConnectHasRootPrivilegnes(BaseTest):
+
+    def setUp(self):
+        self.storage_target_terminal = SSHTerminal(StorageTargetConfig())
+        self.ipu_storage_terminal = SSHTerminal(IPUStorageConfig())
+        self.host_target_terminal = SSHTerminal(HostTargetConfig())
+
+    def runTest(self):
+        self.assertEqual(
+            self.storage_target_terminal.execute("sudo whoami")[0],
+            "root",
+        )
+        self.assertEqual(
+            self.ipu_storage_terminal.execute("sudo whoami")[0],
+            "root",
+        )
+        self.assertEqual(
+            self.host_target_terminal.execute("sudo whoami")[0],
+            "root",
+        )
 
     def tearDown(self):
         pass
