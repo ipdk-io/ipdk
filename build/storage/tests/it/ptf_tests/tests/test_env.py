432d4f090b7630e79514ea15fe853550f2350e66
bartlomiejx <bartlomiejx.kuczma@intel.com>
2022-12-15 10:15:45 +0100

    testFio

--- build/storage/tests/it/ptf_tests/tests/test_env.py
+++ build/storage/tests/it/ptf_tests/tests/test_env.py
@@ -1,113 +1,113 @@
-# Copyright (C) 2022 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
+# # Copyright (C) 2022 Intel Corporation
+# # SPDX-License-Identifier: Apache-2.0
+# #
 #
-
-import os
-
-from ptf.base_tests import BaseTest
-from system_tools.config import (HostTargetConfig, IPUStorageConfig,
-                                 StorageTargetConfig)
-from system_tools.docker import (CMDSenderContainer, HostTargetContainer,
-                                 IPUStorageContainer, StorageTargetContainer)
-from system_tools.ssh_terminal import SSHTerminal
-
-
-class TestTerminalConnect(BaseTest):
-    def setUp(self):
-        self.storage_target_terminal = SSHTerminal(StorageTargetConfig())
-        self.ipu_storage_terminal = SSHTerminal(IPUStorageConfig())
-        self.host_target_terminal = SSHTerminal(HostTargetConfig())
-
-    def runTest(self):
-        self.assertEqual(
-            self.storage_target_terminal.execute("whoami"),
-            self.storage_target_terminal.config.username,
-        )
-        self.assertEqual(
-            self.ipu_storage_terminal.execute("whoami"),
-            self.ipu_storage_terminal.config.username,
-        )
-        self.assertEqual(
-            self.host_target_terminal.execute("whoami"),
-            self.host_target_terminal.config.username,
-        )
-
-    def tearDown(self):
-        pass
-
-
-class TestTerminalConnectHasRootPrivileges(BaseTest):
-    def setUp(self):
-        self.storage_target_terminal = SSHTerminal(StorageTargetConfig())
-        self.ipu_storage_terminal = SSHTerminal(IPUStorageConfig())
-        self.host_target_terminal = SSHTerminal(HostTargetConfig())
-
-    def runTest(self):
-        self.assertEqual(
-            self.storage_target_terminal.execute("sudo whoami"),
-            "root",
-        )
-        self.assertEqual(
-            self.ipu_storage_terminal.execute("sudo whoami"),
-            "root",
-        )
-        self.assertEqual(
-            self.host_target_terminal.execute("sudo whoami"),
-            "root",
-        )
-
-    def tearDown(self):
-        pass
-
-
-class TestDeployContainers(BaseTest):
-    def setUp(self):
-        self.storage_target_terminal = SSHTerminal(StorageTargetConfig())
-        self.ipu_storage_terminal = SSHTerminal(IPUStorageConfig())
-        self.host_target_terminal = SSHTerminal(HostTargetConfig())
-
-        self.branch = "main"
-        self.repository_url = "'https://github.com/ipdk-io/ipdk.git'"
-        self.shared_dir = os.path.join(HostTargetConfig().vm_share_dir_path, "shared")
-
-    def runTest(self):
-        self.storage_target_terminal.execute(
-            f"sudo rm -rf {self.storage_target_terminal.config.workdir}"
-        )
-        self.storage_target_terminal.execute(
-            f"mkdir {self.storage_target_terminal.config.workdir}"
-        )
-
-        cmd = f"cd {self.storage_target_terminal.config.workdir} && git clone --branch {self.branch} {self.repository_url}"
-        self.storage_target_terminal.execute(cmd)
-
-        self.storage_target_terminal.execute(
-            f"ls {self.storage_target_terminal.config.workdir}/ipdk/build"
-        )
-        self.storage_target_terminal.execute(
-            f"cd {self.storage_target_terminal.config.storage_dir} && git log"
-        )
-
-        self.storage_target_container = StorageTargetContainer()
-        self.assertIn(
-            "storage-target", self.storage_target_terminal.execute("docker ps")
-        )
-
-        self.ipu_storage_terminal.client.exec_command(f"mkdir -p {self.shared_dir}")
-        self.ipu_storage_container = IPUStorageContainer()
-        self.assertIn(
-            "ipu-storage-container", self.ipu_storage_terminal.execute("docker ps")
-        )
-
-        self.host_target_container = HostTargetContainer()
-        # container runs by the command below and stopped immediately after cmd execution
-        self.assertIn("host-target", self.host_target_terminal.execute("docker ps -a"))
-
-        self.cmd_sender_container = CMDSenderContainer(IPUStorageConfig())
-        self.assertIn("cmd-sender", self.ipu_storage_terminal.execute("docker ps"))
-
-    def tearDown(self):
-        self.storage_target_container.stop()
-        self.host_target_container.stop()
-        self.ipu_storage_container.stop()
-        self.cmd_sender_container.stop()
+# import os
+#
+# from ptf.base_tests import BaseTest
+# from system_tools.config import (HostTargetConfig, IPUStorageConfig,
+#                                  StorageTargetConfig)
+# from system_tools.docker import (CMDSenderContainer, HostTargetContainer,
+#                                  IPUStorageContainer, StorageTargetContainer)
+# from system_tools.ssh_terminal import SSHTerminal
+#
+#
+# class TestTerminalConnect(BaseTest):
+#     def setUp(self):
+#         self.storage_target_terminal = SSHTerminal(StorageTargetConfig())
+#         self.ipu_storage_terminal = SSHTerminal(IPUStorageConfig())
+#         self.host_target_terminal = SSHTerminal(HostTargetConfig())
+#
+#     def runTest(self):
+#         self.assertEqual(
+#             self.storage_target_terminal.execute("whoami"),
+#             self.storage_target_terminal.config.username,
+#         )
+#         self.assertEqual(
+#             self.ipu_storage_terminal.execute("whoami"),
+#             self.ipu_storage_terminal.config.username,
+#         )
+#         self.assertEqual(
+#             self.host_target_terminal.execute("whoami"),
+#             self.host_target_terminal.config.username,
+#         )
+#
+#     def tearDown(self):
+#         pass
+#
+#
+# class TestTerminalConnectHasRootPrivileges(BaseTest):
+#     def setUp(self):
+#         self.storage_target_terminal = SSHTerminal(StorageTargetConfig())
+#         self.ipu_storage_terminal = SSHTerminal(IPUStorageConfig())
+#         self.host_target_terminal = SSHTerminal(HostTargetConfig())
+#
+#     def runTest(self):
+#         self.assertEqual(
+#             self.storage_target_terminal.execute("sudo whoami"),
+#             "root",
+#         )
+#         self.assertEqual(
+#             self.ipu_storage_terminal.execute("sudo whoami"),
+#             "root",
+#         )
+#         self.assertEqual(
+#             self.host_target_terminal.execute("sudo whoami"),
+#             "root",
+#         )
+#
+#     def tearDown(self):
+#         pass
+#
+#
+# class TestDeployContainers(BaseTest):
+#     def setUp(self):
+#         self.storage_target_terminal = SSHTerminal(StorageTargetConfig())
+#         self.ipu_storage_terminal = SSHTerminal(IPUStorageConfig())
+#         self.host_target_terminal = SSHTerminal(HostTargetConfig())
+#
+#         self.branch = "main"
+#         self.repository_url = "'https://github.com/ipdk-io/ipdk.git'"
+#         self.shared_dir = os.path.join(HostTargetConfig().vm_share_dir_path, "shared")
+#
+#     def runTest(self):
+#         self.storage_target_terminal.execute(
+#             f"sudo rm -rf {self.storage_target_terminal.config.workdir}"
+#         )
+#         self.storage_target_terminal.execute(
+#             f"mkdir {self.storage_target_terminal.config.workdir}"
+#         )
+#
+#         cmd = f"cd {self.storage_target_terminal.config.workdir} && git clone --branch {self.branch} {self.repository_url}"
+#         self.storage_target_terminal.execute(cmd)
+#
+#         self.storage_target_terminal.execute(
+#             f"ls {self.storage_target_terminal.config.workdir}/ipdk/build"
+#         )
+#         self.storage_target_terminal.execute(
+#             f"cd {self.storage_target_terminal.config.storage_dir} && git log"
+#         )
+#
+#         self.storage_target_container = StorageTargetContainer()
+#         self.assertIn(
+#             "storage-target", self.storage_target_terminal.execute("docker ps")
+#         )
+#
+#         self.ipu_storage_terminal.client.exec_command(f"mkdir -p {self.shared_dir}")
+#         self.ipu_storage_container = IPUStorageContainer()
+#         self.assertIn(
+#             "ipu-storage-container", self.ipu_storage_terminal.execute("docker ps")
+#         )
+#
+#         self.host_target_container = HostTargetContainer()
+#         # container runs by the command below and stopped immediately after cmd execution
+#         self.assertIn("host-target", self.host_target_terminal.execute("docker ps -a"))
+#
+#         self.cmd_sender_container = CMDSenderContainer(IPUStorageConfig())
+#         self.assertIn("cmd-sender", self.ipu_storage_terminal.execute("docker ps"))
+#
+#     def tearDown(self):
+#         self.storage_target_container.stop()
+#         self.host_target_container.stop()
+#         self.ipu_storage_container.stop()
+#         self.cmd_sender_container.stop()
---
+++ build/storage/tests/it/ptf_tests/tests/test_fio.py
@@ -0,0 +1,70 @@
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from ptf.base_tests import BaseTest
+from system_tools.config import TestConfig
+from system_tools.test_platform import PlatformFactory
+
+
+class Fio(BaseTest):
+    def setUp(self):
+        self.tests_config = TestConfig()
+        self.platforms_factory = PlatformFactory(self.tests_config.cmd_sender_platform)
+        self.storage_target_platform = (
+            self.platforms_factory.create_storage_target_platform()
+        )
+        self.ipu_storage_platform = self.platforms_factory.create_ipu_storage_platform()
+        self.host_target_platform = self.platforms_factory.create_host_target_platform()
+
+    def runTest(self):
+        self.assertTrue(
+            self.storage_target_platform.is_port_free(self.tests_config.nvme_port)
+        )
+        self.storage_target_platform.create_subsystem(
+            self.tests_config.nqn,
+            self.tests_config.nvme_port,
+            self.tests_config.spdk_port,
+        )
+        self.assertFalse(
+            self.storage_target_platform.is_port_free(self.tests_config.nvme_port)
+        )
+        self.assertTrue(
+            self.storage_target_platform.is_app_listening_on_port(
+                "spdk_tgt", self.tests_config.nvme_port
+            )
+        )
+
+        remote_nvme_storages = self.storage_target_platform.create_ramdrives(
+            self.tests_config.min_ramdrive,
+            self.tests_config.nvme_port,
+            self.tests_config.nqn,
+            self.tests_config.spdk_port,
+        )
+
+        self.assertEqual(len(remote_nvme_storages), self.tests_config.min_ramdrive)
+
+        self.assertEqual(
+            self.host_target_platform.get_number_of_virtio_blk_devices(), 0
+        )
+        devices_handles = (
+            self.ipu_storage_platform.create_virtio_blk_devices_sequentially(
+                self.host_target_platform.get_service_address(),
+                remote_nvme_storages,
+            )
+        )
+        cmd_sender_id = self.ipu_storage_platform.cmd_sender.id
+        cmd = f"""docker exec {cmd_sender_id} grpc_cli call {self.ipu_storage_platform.get_ip_address()}:50051 RunFio""" \
+              f""" "diskToExercise: {{ deviceHandle: '{devices_handles[0]._device_handle}' }} fioArgs: """ \
+              f"""'{{\\"rw\\":\\"randrw\\", \\"runtime\\":1, \\"numjobs\\": 1, \\"time_based\\": 1, """ \
+              f"""\\"group_reporting\\": 1 }}'" """
+
+        x = self.ipu_storage_platform.terminal.execute(cmd)
+        print(x)
+        return x
+
+    def tearDown(self):
+        self.ipu_storage_platform.clean()
+        self.storage_target_platform.clean()
+        self.host_target_platform.clean()
+
