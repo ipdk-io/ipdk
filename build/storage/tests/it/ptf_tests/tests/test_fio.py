17712bc16a2f3c21f61417e496c74e53726cd817
bartlomiejx <bartlomiejx.kuczma@intel.com>
2022-12-15 14:03:09 +0100

    Update test_fio.py

--- build/storage/tests/it/ptf_tests/tests/test_fio.py
+++ build/storage/tests/it/ptf_tests/tests/test_fio.py
@@ -54,19 +54,19 @@ class Fio(BaseTest):
             )
         )
 
-        for device in devices_handles:
-            device.run_fio()
+        # for device in devices_handles:
+        #     device.run_fio()
 
         # devices_handles[0].run_fio(self.ipu_storage_platform)
-        # cmd_sender_id = self.ipu_storage_platform.cmd_sender.id
-        # cmd = f"""docker exec {cmd_sender_id} grpc_cli call {self.ipu_storage_platform.get_ip_address()}:50051 RunFio""" \
-        #       f""" "diskToExercise: {{ deviceHandle: '{devices_handles[0]._device_handle}' }} fioArgs: """ \
-        #       f"""'{{\\"rw\\":\\"randrw\\", \\"runtime\\":1, \\"numjobs\\": 1, \\"time_based\\": 1, """ \
-        #       f"""\\"group_reporting\\": 1 }}'" """
+        cmd_sender_id = self.ipu_storage_platform.cmd_sender.id
+        cmd = f"""docker exec {cmd_sender_id} grpc_cli call {self.ipu_storage_platform.get_ip_address()}:50051 RunFio""" \
+              f""" "diskToExercise: {{ deviceHandle: '{devices_handles[0]._device_handle}' }} fioArgs: """ \
+              f"""'{{\\"rw\\":\\"randrw\\", \\"runtime\\":1, \\"numjobs\\": 1, \\"time_based\\": 1, """ \
+              f"""\\"group_reporting\\": 1 }}'" """
 
-        # x = self.ipu_storage_platform.terminal.execute(cmd)
-        # print(x)
-        # return x
+        x = self.ipu_storage_platform.terminal.execute(cmd)
+        print(x)
+        return x
 
     def tearDown(self):
         self.ipu_storage_platform.clean()
