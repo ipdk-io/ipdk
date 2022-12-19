53811f1433e64804d680b1794f2cff7291c5de31
bartlomiejx <bartlomiejx.kuczma@intel.com>
2022-12-19 17:44:22 +0100

    Update test_fio.py

--- build/storage/tests/it/ptf_tests/tests/test_fio.py
+++ build/storage/tests/it/ptf_tests/tests/test_fio.py
@@ -18,21 +18,11 @@ class Fio(BaseTest):
         self.host_target_platform = self.platforms_factory.create_host_target_platform()
 
     def runTest(self):
-        self.assertTrue(
-            self.storage_target_platform.is_port_free(self.tests_config.nvme_port)
-        )
+
         self.storage_target_platform.create_subsystem(
             self.tests_config.nqn,
             self.tests_config.nvme_port,
             self.tests_config.spdk_port,
-        )
-        self.assertFalse(
-            self.storage_target_platform.is_port_free(self.tests_config.nvme_port)
-        )
-        self.assertTrue(
-            self.storage_target_platform.is_app_listening_on_port(
-                "spdk_tgt", self.tests_config.nvme_port
-            )
         )
 
         remote_nvme_storages = self.storage_target_platform.create_ramdrives(
@@ -42,11 +32,6 @@ class Fio(BaseTest):
             self.tests_config.spdk_port,
         )
 
-        self.assertEqual(len(remote_nvme_storages), self.tests_config.min_ramdrive)
-
-        self.assertEqual(
-            self.host_target_platform.get_number_of_virtio_blk_devices(), 0
-        )
         devices_handles = (
             self.ipu_storage_platform.create_virtio_blk_devices_sequentially(
                 self.host_target_platform.get_service_address(),
@@ -61,8 +46,7 @@ class Fio(BaseTest):
             fio_with_params = x.run_fio_with_params(mode=mode, runtime=1, numjobs=1, time_based=1, group_reporting=1)
             print("FIO: "+mode + "\n")
             print(fio_with_params)
-
-            self.assertIn("min=", fio_with_params)
+            self.result[mode] = fio_with_params
         # for device in devices_handles:
         #     fio = device.run_fio()
         #     fio_with_params = device.run_fio_with_params(mode="randrw", runtime=1, numjobs=1, time_based=1,
@@ -72,7 +56,8 @@ class Fio(BaseTest):
         #     print(fio_with_params)
             # self.assertIn("err= 0", fio)
 
-            self.assertIn("err= 0", fio_with_params)
+        assert "read" in self.result["randrw"]
+
 
     def tearDown(self):
         self.ipu_storage_platform.clean()
